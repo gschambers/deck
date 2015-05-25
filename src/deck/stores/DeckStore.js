3358f12d343c3d74028d5952e4780aa88130932b
Gary Chambers <gary@lipsmack.co.uk>
2015-05-25 22:59:46 +0100

    Remove card from hand after placement

--- src/deck/stores/DeckStore.js
+++ src/deck/stores/DeckStore.js
@@ -1,5 +1,7 @@
 import { List } from "immutable";
 import { BehaviorSubject } from "rx";
+
+import { observeActions } from "../../common/helpers/action";
 import { wrap } from "../../common/helpers/observe";
 import Attribute from "../models/Attribute";
 import Card from "../models/Card";
@@ -39,3 +41,27 @@ const { observe } = wrap(state);
 export {
     observe
 };
+
+/**
+ * @param {String} deckName
+ * @param {Map} data
+ * @return {List<Deck>}
+ */
+function handleRemoveCardFromDeck(deckName, data) {
+    const decks = state.value;
+
+    const index = decks.findIndex(
+        deck => deck.name === deckName
+    );
+
+    return decks.updateIn(
+        [index, "cards"],
+        cards => cards.filterNot(
+            card => card === data.get("card")
+        )
+    );
+}
+
+observeActions("board", "placeCard")
+    .map(action => handleRemoveCardFromDeck("hand", action.data))
+    .forEach(value => state.onNext(value));
